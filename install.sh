baf257a2fb21a4feb03b2c1f731559fafb0315c7
zadachnik <40857994+zadachnik@users.noreply.github.com>
2019-08-17 08:09:31 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-
 clear
-
 sudo apt update
-
 sudo apt install python3
-
 sudo apt install python3-pip
-
 pip install scapy
-
 pip install shodan
-
 pip3 install -r requirements.txt
-
 python3 Memcrashed.py
-

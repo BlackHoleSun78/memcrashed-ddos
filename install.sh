f5f54ecb63a3d81eb3edf7a5fdffb84c540feea5
zadachnik <40857994+zadachnik@users.noreply.github.com>
2019-08-17 08:06:40 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-apt update
-apt install python3
-apt install python3-pip
+
+clear
+
+sudo apt update
+
+sudo apt install python3
+
+sudo apt install python3-pip
+
 pip install scapy
+
 pip install shodan
+
 pip3 install -r requirements.txt
+
 python3 Memcrashed.py
+
